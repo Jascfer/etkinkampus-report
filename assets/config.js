8bb00b3a8c9354e7939d532dc6ac2097ccc7204f
Jascfer <92553074+Jascfer@users.noreply.github.com>
2025-09-23 02:12:44 +0300

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -1,6 +1,6 @@
 // assets/config.js
 // TODO: Aşağıdaki URL'yi kendi Google Apps Script Web App uç noktanla değiştir.
-const API_URL = "https://script.google.com/macros/s/AKfycbzN6N1fDFRhnnzivuZxsuJ-3lyOpZbsXw2hwdp5zbyXx4nATnHe2p73R6kxClOiQNvDTw/exec"; // örnek
+const API_URL = "https://script.google.com/macros/s/AKfycbwD31sHjMqgIYLEAskhVKHHsEJtLW5KuUgZSkkUX4GbT3nAt8qqZBHLlpOa3laNezutoQ/exec"; // örnek
 // Otomatik yenileme süresi (saniye)
 const AUTO_REFRESH_SEC = 60;
 // Sayfa başına gösterilecek kayıt sayısı
